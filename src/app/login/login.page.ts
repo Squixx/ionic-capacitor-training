fab562d5de39c2771ee1b015720c1d19ec388c67
M van der Meij <martijn.vandermeij@vattenfall.com>
2023-01-31 15:35:42 +0100

    add login page

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  email: string = '';
+  password: string = '';
+
+  constructor() {}
+  signIn() {
+    console.log(this.email, this.password);
+  }
+}
